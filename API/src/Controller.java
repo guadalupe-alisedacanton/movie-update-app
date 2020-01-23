aa69fda1642127d2e14f46f6b5c3105b338e3f87
s-alisedag <s-alisedag@53S50029749.it.bsd405.org>
2020-01-23 14:04:08 -0800

    First attempt at MVC Framework

---
+++ API/src/Controller.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Controller {
+	private Main mainCode;
+	private MovieGUI view;
+	
+	public Controller(Main mainCode, MovieGUI view) {
+		this.mainCode = mainCode;
+		this.view = view;
+		
+	}
+
+}
--- API/src/Main.java
+++ API/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -16,110 +18,24 @@ import org.json.JSONObject;
 
 public class Main {
   private static HttpURLConnection connection;
-  public static ArrayList<String> titles;
-  public static ArrayList<String> overviews;
-  public static ArrayList<String> genres;
-  public static ArrayList<String> posters;
-  
+   
+  /*
+  public void retrieveMovieList() {
 
-  public static void main(String[] args) {
-	  //List of now playing movies and upcoming movies
-	  titles = new ArrayList<>();
-	  overviews = new ArrayList<>();
-	  genres = new ArrayList<>();
-	  posters = new ArrayList<>();
+
+
+
+	//List of now playing movies and upcoming movies
+	  //String titles[] = new String[100];
+	  ArrayList<Movie> movies = new ArrayList<>();
 	  HashMap<String, Integer> genreIds = new HashMap<String, Integer>();
 	  
 	  
 	  String apiKey = "709c731348589bc63268f0736fe09578";	
 	  String baseUrl = "https://api.themoviedb.org";
     
-    //Method 1: java.net.HTTPURLConnection
-	//GENRES
-	    //
-	    //
-	    //
-	    //
-	    //
-	    //
-	    //
-	    BufferedReader newReader;
-	    String newLine;
-	    StringBuffer newResponseContent = new StringBuffer();
-	    
-	    
-	    try {
-	    	//String upcomingMovies = "/3/movie/upcoming";
-	    	String getGenres = "/3/genre/movie/list";
-	    	URL url =  new URL(baseUrl + getGenres + "?api_key=" + apiKey);
-	          	
-	    	connection = (HttpURLConnection) url.openConnection();
-
-	      //Request setup
-	      connection.setRequestMethod("GET");
-	      connection.setConnectTimeout(5000);
-	      connection.setReadTimeout(5000);
-	      
-	      
-	      
-	      int status = connection.getResponseCode();
-	      
-//	      System.out.println(status);
-	      
-
-	      
-	      if (status > 299) {
-	        newReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-	        while ((newLine = newReader.readLine()) != null) {
-	          newResponseContent.append(newLine);
-	          
-	        }
-	        
-	        newReader.close();
-	        
-	      } else {
-	    	  
-	        newReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-	        while ((newLine = newReader.readLine()) != null) {
-	          newResponseContent.append(newLine);
-	          
-	        }
-	        
-	        newReader.close();
-	      }
-
-	      JSONObject output = new JSONObject(newResponseContent.toString());
-//	      System.out.println(output.toString(4));
-	      
-	      
-		  JSONArray genreArr = output.getJSONArray("genres");
-		  
-		  for (int i = 0; i < genreArr.length(); i++) {
-			  String genreName = genreArr.getJSONObject(i).getString("name");
-			  int id = genreArr.getJSONObject(i).getInt("id");
-
-			  genreIds.put(genreName, id);
-			  //System.out.println("Genre: " + genreName + ", Id: " + genreIds.get(genreName));
-		  }
-		  
-		  
-
-	    } catch (MalformedURLException e) {
-	      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    } finally {
-	      connection.disconnect();
-	    }
+ 
 	  
-	  
-	  
-	//MOVIES
-	//
-	//
-	 //
-	//
-	//    
     BufferedReader reader;
     String line;
     StringBuffer responseContent = new StringBuffer();
@@ -136,13 +52,112 @@ public class Main {
       connection.setConnectTimeout(5000);
       connection.setReadTimeout(5000);
       
-      
-      
+
       int status = connection.getResponseCode();
       
 //      System.out.println(status);
       
+      if (status > 299) {
+        reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        while ((line = reader.readLine()) != null) {
+          responseContent.append(line);
+          
+        }
+        
+        reader.close();
+        
+      } else {
+    	  
+        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        while ((line = reader.readLine()) != null) {
+          responseContent.append(line);
+          
+        }
+        
+        reader.close();
+      }
+        
+	  
+	 //entire output
+      JSONObject output = new JSONObject(responseContent.toString());
+//      System.out.println(output.toString(4));
+      //results is just the movies and their info
+	  JSONArray movieInfoArr = output.getJSONArray("results");
+	  
+	  //this for loop pairs the genre with its id since the results gives only genre ids not genres
+	  JSONArray genreArr = output.getJSONArray("genres");
+      for (int i = 0; i < genreArr.length(); i++) {
+		  String genreName = genreArr.getJSONObject(i).getString("name");
+		  int id = genreArr.getJSONObject(i).getInt("id");
 
+		  genreIds.put(genreName, id);
+		  //System.out.println("Genre: " + genreName + ", Id: " + genreIds.get(genreName));
+	  }
+	  
+      //looks through the movies and takes out the title, overview, and genre of each movie
+      //creates a new Movie object and adds it to a list of Movies.
+	  for (int i = 0; i < movieInfoArr.length(); i++) {
+
+		  String title = movieInfoArr.getJSONObject(i).getString("title");
+		  String overview = movieInfoArr.getJSONObject(i).getString("overview");
+		  JSONArray movieGenreIDs = movieInfoArr.getJSONObject(i).getJSONArray("genre_ids");
+		  String genre = null;
+		  for (String key : genreIds.keySet()) {
+			  if (movieGenreIDs.getInt(0) == genreIds.get(key)) {
+				  genre = key;
+				  
+			  }
+		  }
+		  
+		  movies.add(new Movie(title, genre, overview));
+		  
+		  //String title = movieInfoArr.getJSONObject(i).getString("title");
+		  //titles[i] = (title);
+		  
+		 
+	  }
+	  
+	  
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      connection.disconnect();
+    }
+    
+  
+  
+  }
+  
+  
+
+  public String[] retrievePlotSummary() {
+
+
+	  String overviews[] = new String[100];
+	  String apiKey = "709c731348589bc63268f0736fe09578";	
+	  String baseUrl = "https://api.themoviedb.org";
+	  BufferedReader reader;
+	  String line;
+	  StringBuffer responseContent = new StringBuffer();
+    
+    try {
+    	
+    	String nowPlaying = "/3/movie/now_playing";
+    	URL url = new URL(baseUrl + nowPlaying + "?api_key=" + apiKey);     
+      	
+    	connection = (HttpURLConnection) url.openConnection();
+
+      //Request setup
+      connection.setRequestMethod("GET");
+      connection.setConnectTimeout(5000);
+      connection.setReadTimeout(5000);
+      
+
+      int status = connection.getResponseCode();
+      
+//      System.out.println(status);
       
       if (status > 299) {
         reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
@@ -166,44 +181,15 @@ public class Main {
 
       JSONObject output = new JSONObject(responseContent.toString());
 //      System.out.println(output.toString(4));
-      
-      
-      
+
 	  JSONArray movieInfoArr = output.getJSONArray("results");
 	  
-	  
-	  
 	  for (int i = 0; i < movieInfoArr.length(); i++) {
-		  String title = movieInfoArr.getJSONObject(i).getString("title");
-		  titles.add(title);
-		  
+		 
 		  String overview = movieInfoArr.getJSONObject(i).getString("overview");
-		  overviews.add(overview);
-		  
-		  String poster_path = movieInfoArr.getJSONObject(i).getString("poster_path");
-		  posters.add(poster_path);
-		  
-		  
-		  JSONArray movieGenreIDs = movieInfoArr.getJSONObject(i).getJSONArray("genre_ids");
-		  
-		  for (String key : genreIds.keySet()) {
-			  if (movieGenreIDs.getInt(0) == genreIds.get(key)) {
-				  genres.add(key);
-			  }
-		  }
-
+		  overviews[i] = (overview);
 	  }
 	  
-//	  for (int j = 0; j < genres.size(); j++) {
-//		  System.out.println(titles.get(j));
-//		  System.out.println(overviews.get(j));
-//		  System.out.println(genres.get(j));
-//	  }
-	  
-	  
-	  
-	  
-
     } catch (MalformedURLException e) {
       e.printStackTrace();
     } catch (IOException e) {
@@ -212,8 +198,158 @@ public class Main {
       connection.disconnect();
     }
     
-    
-    //
-  
-  }  
+    return overviews;
+  }
+*/
+
+  public static HashMap<String, Integer> retrieveGenre() {
+
+	  	String apiKey = "709c731348589bc63268f0736fe09578";	
+		String baseUrl = "https://api.themoviedb.org";
+		HashMap<String, Integer> genreIds = new HashMap<String, Integer>();
+	  	
+	  	
+	    BufferedReader newReader;
+	    String newLine;
+	    StringBuffer newResponseContent = new StringBuffer();
+
+	    try {
+	    	//String upcomingMovies = "/3/movie/upcoming";
+	    	String getGenres = "/3/genre/movie/list";
+	    	URL url =  new URL(baseUrl + getGenres + "?api_key=" + apiKey);
+	          	
+	    	connection = (HttpURLConnection) url.openConnection();
+
+	      //Request setup
+	      connection.setRequestMethod("GET");
+	      connection.setConnectTimeout(5000);
+	      connection.setReadTimeout(5000);
+	      
+	      int status = connection.getResponseCode();
+	      
+//	      System.out.println(status);
+	   
+	      if (status > 299) {
+	        newReader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+	        while ((newLine = newReader.readLine()) != null) {
+	          newResponseContent.append(newLine);
+	          
+	        }
+	        
+	        newReader.close();
+	        
+	      } else {
+	    	  
+	        newReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	        while ((newLine = newReader.readLine()) != null) {
+	          newResponseContent.append(newLine);
+	          
+	        }
+	        
+	        newReader.close();
+	      }
+	      JSONObject output = new JSONObject(newResponseContent.toString());
+	      JSONArray genreArr = output.getJSONArray("genres");
+		  
+		  for (int i = 0; i < genreArr.length(); i++) {
+			  String genreName = genreArr.getJSONObject(i).getString("name");
+			  int id = genreArr.getJSONObject(i).getInt("id");
+
+			  genreIds.put(genreName, id);
+			  //System.out.println("Genre: " + genreName + ", Id: " + genreIds.get(genreName));
+		  }
+	      
+	     
+//	      System.out.println(output.toString(4));
+	      
+
+	    } catch (MalformedURLException e) {
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    } finally {
+	      connection.disconnect();
+	    }
+	  
+	  return genreIds;
 }
+  public static void main(String[] args) {
+	//List of now playing movies and upcoming movies	  
+	  ArrayList<Movie> movies = new ArrayList<>();
+	  HashMap<String, Integer> genreIds = retrieveGenre();
+	  
+	  String apiKey = "709c731348589bc63268f0736fe09578";	
+	  String baseUrl = "https://api.themoviedb.org";
+    
+	  BufferedReader reader;
+	  String line;
+	  StringBuffer responseContent = new StringBuffer();
+    
+	  try {
+		  String nowPlaying = "/3/movie/now_playing";
+		  URL url = new URL(baseUrl + nowPlaying + "?api_key=" + apiKey);     
+		  connection = (HttpURLConnection) url.openConnection();
+
+		  //Request setup
+		  connection.setRequestMethod("GET");
+		  connection.setConnectTimeout(5000);
+		  connection.setReadTimeout(5000);
+      
+		  int status = connection.getResponseCode();   //System.out.println(status);
+      
+		  if (status > 299) {
+			  
+			  reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+			  while ((line = reader.readLine()) != null) {
+				  responseContent.append(line);
+				  
+			  }
+			  reader.close();
+        
+		  } else {
+    	  
+			  reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			  while ((line = reader.readLine()) != null) {
+				  responseContent.append(line);
+          
+			  }
+			  reader.close();
+			  
+		  }
+		  //entire output
+		  JSONObject output = new JSONObject(responseContent.toString());
+		  //System.out.println(output.toString(4));
+		  //results is just the movies and their info
+		  JSONArray movieInfoArr = output.getJSONArray("results");
+	
+		  //looks through the movies and takes out the title, overview, and genre of each movie
+		  //creates a new Movie object and adds it to a list of Movies.
+		  for (int i = 0; i < movieInfoArr.length(); i++) {
+
+			  String title = movieInfoArr.getJSONObject(i).getString("title");
+			  String overview = movieInfoArr.getJSONObject(i).getString("overview");
+			  JSONArray movieGenreIDs = movieInfoArr.getJSONObject(i).getJSONArray("genre_ids");
+			  String genre;
+			  for (String key : genreIds.keySet()) {
+				  if (movieGenreIDs.getInt(0) == genreIds.get(key)) {
+					  genre = key;
+					  movies.add(new Movie(title, genre, overview));
+					  
+				  }
+			  
+			  }
+			  //System.out.println(movies.get(0).getGenre());	  
+		 
+		  }
+		  
+	  } catch (MalformedURLException e) {
+		  e.printStackTrace();
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  } finally {
+		  connection.disconnect();
+	  }
+  
+  	}  
+}
+
